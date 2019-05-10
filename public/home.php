f479c30b4db4fa252c3e73300783cbc221b69d2e
Aitor <aitor.1999@hotmail.com>
2019-05-10 12:57:14 +0200

    muestra chuches

--- public/home.php
+++ public/home.php
@@ -9,8 +9,24 @@
 <body>
      <!-- Menu Navegacion -->
     <?php include "./assets/navbar.php"?>
-
+<hr>
     <h2 class="chuchemes">Chuche del mes</h2>
     <img src="images/chuchemes.jpg">
+<hr>
+<?php
+echo '<div class="row">';
+while ($chuche = mysql_fetch_assoc($resultado)) {
+    
+    echo '<div class="chuchediv">';
+    echo '<img src="images/$chuche[id].png">';
+    echo '<span><a href="chuche.php?id=$chuche[id]">'.$chuche['nombre'].'</a></span>';
+    echo '</div>';
+    if($i == 3)
+    echo '<div class="row"></div>';
+    else
+    echo '</div>';    
+}
+echo '</div>';
+?>
 </body>
 </html>
